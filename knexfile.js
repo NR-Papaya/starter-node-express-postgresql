4517a960a996f4344f41859b4bbc1638def29c38
Nahia Riviera <n.riviera.dev@gmail.com>
2022-06-06 11:50:25 -0500

    working with knex

---
+++ knexfile.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const path = require("path");
+const { DATABASE_URL } = process.env;
+
+module.exports = {
+	development: {
+		client: "postgresql",
+		connection: DATABASE_URL,
+		migrations: {
+			directory: path.join(__dirname, "src", "db", "migrations"),
+		},
+	},
+
+	test: {
+		client: "postgresql",
+		pool: { min: 1, max: 5 },
+		connection: {
+			database: "db_test",
+			user: "username_test",
+			password: "password_test",
+		},
+	},
+
+	staging: {
+		client: "postgresql",
+		connection: {
+			database: "db_staging",
+			user: "username_staging",
+			password: "password_staging",
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: "knex_migrations",
+		},
+	},
+
+	production: {
+		client: "postgresql",
+		connection: {
+			database: "db_production",
+			user: "username_production",
+			password: "password_production",
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			tableName: "knex_migrations",
+		},
+	},
+};
